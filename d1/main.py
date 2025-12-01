120b96859de5f8fa1b0390d105a795c89b531db1
Martin Milota <martin.milota@chargedmonkey.com>
2025-12-01 22:01:35 +0100

    Solution part 1 and 2

--- d1/main.py
+++ d1/main.py
@@ -1,5 +1,50 @@
-
-
+from typing import Iterable
+from functools import reduce
 from io import open
 
-with open("input_test.txt", )
+
+def parse(it: Iterable[str]) -> Iterable[int]:
+    for s in it:
+        s = s.replace("R", "").replace("L", "-")
+        yield int(s)
+
+
+def solve_part_one(dial: int, commands: Iterable[int]) -> tuple[int, int]:
+    jackpots = 0
+
+    for i in commands:
+        # print(i)
+        dial = (dial + i) % 100
+
+        if dial == 0:
+            jackpots += 1
+
+    return dial, jackpots
+
+
+def solve_part_two(dial: int, commands: Iterable[int]) -> tuple[int, int]:
+    jackpots = 0
+
+    for i in commands:
+        # print(i)
+        was_null = dial == 0
+        full_rotations = int(i / 100)
+        jackpots += abs(full_rotations)
+        i -= full_rotations * 100 # correction for sum != dial check
+        sum = dial + i
+        dial = sum % 100
+
+        if dial == 0:
+            jackpots += 1
+        elif sum != dial and not was_null:
+            jackpots += 1
+
+    return dial, jackpots
+
+
+with (open("input.txt") as input):
+    lines = input.readlines()
+    commands = parse(lines)
+
+    # print(solve_part_one(50, commands))
+    print(solve_part_two(50, commands))

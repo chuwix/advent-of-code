629a347050981c4202b66b8f1f0d33cad04d6ec3
Martin Milota <martin.milota@chargedmonkey.com>
2025-12-05 09:07:23 +0100

    d5 p1

---
+++ y2025/d5/main.py
@@ -0,0 +1,79 @@
+from dataclasses import dataclass
+from io import open
+
+
+@dataclass
+class Range:
+    lower: int
+    upper: int
+
+    def contains(self, id: int) -> bool:
+        return self.lower <= id <= self.upper
+
+
+def parse(s: str) -> Range:
+    vals = s.split("-", 2)
+    return Range(int(vals[0]), int(vals[1]))
+
+
+def get_ranges_and_ids(file: str) -> tuple[list[Range], list[int]]:
+    ranges, ids = [], []
+
+    with (open(file) as input):
+        for row in input:
+            if row.isspace():
+                break
+            ranges.append(parse(row))
+
+        for row in input:
+            ids.append(int(row))
+
+    return ranges, ids
+
+
+def is_overlap_sorted(smaller: Range, latter: Range) -> bool:
+    return latter.lower <= smaller.upper
+
+
+def merge_ranges(ranges: list[Range]) -> list[Range]:
+    ranges.sort(key=lambda r: r.lower)
+    ranges_iter = iter(ranges)
+    merged_ranges = [next(ranges_iter)]
+
+    for range in ranges:
+        last_range = merged_ranges[-1]
+
+        if is_overlap_sorted(last_range, range):
+            merged_ranges[-1].upper = max(last_range.upper, range.upper)
+        else:
+            merged_ranges.append(range)
+
+    return merged_ranges
+
+
+def count_fresh_ids(merged_ranges: list[Range], ids: list[int]) -> int:
+    ids.sort()
+    fresh_count = 0
+    merged_ranges_iter = iter(merged_ranges)
+    current_range = next(merged_ranges_iter)
+
+    for id in ids:
+        while current_range.upper < id:
+            try:
+                current_range = next(merged_ranges_iter)
+            except StopIteration:
+                break
+
+        if current_range.contains(id):
+            fresh_count += 1
+
+    return fresh_count
+
+
+def solve_part_one(ranges: list[Range], ids: list[int]) -> int:
+    merged_ranges = merge_ranges(ranges)
+    return count_fresh_ids(merged_ranges, ids)
+
+
+def solve_part_two(ranges: list[Range], ids: list[int]) -> int:
+    pass
---
+++ y2025/d5/test.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+from y2025.d5.main import solve_part_one, solve_part_two, get_ranges_and_ids
+
+TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'input_test.txt')
+
+
+class Test(unittest.TestCase):
+    def test_part_one(self):
+        ranges, ids = get_ranges_and_ids(TESTDATA_FILENAME)
+        fresh_ids = solve_part_one(ranges, ids)
+        # print(list(invalid_ids))
+        self.assertEqual(3, fresh_ids)
+
+    # def test_part_two(self):
+    #     ranges = get_and_transform_data(TESTDATA_FILENAME)
+    #     invalid_ids = solve_part_two(ranges)
+    #     # print(list(invalid_ids))
+    #     self.assertEqual(4174379265, sum(invalid_ids))
+
+
+if __name__ == "__main__":
+    unittest.main()

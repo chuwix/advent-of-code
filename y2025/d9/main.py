d745cd648906d61c16321f7976262dacc585f18f
Martin Milota <martin.milota@chargedmonkey.com>
2025-12-09 23:49:27 +0100

    Flood filling - wont run in timely manner, sigh

--- y2025/d9/main.py
+++ y2025/d9/main.py
@@ -3,8 +3,10 @@ from __future__ import annotations
 from typing import Iterable
 
 from more_itertools.more import distinct_combinations
+from more_itertools.recipes import prepend, triplewise, pairwise, flatten
 
-from tools.datastructures.points import Point2
+from tools.datastructures.points import Point2, Rectangle
+from tools.math import sign
 
 
 def parse_inputs(file) -> Iterable[Point2]:
@@ -14,10 +16,51 @@ def parse_inputs(file) -> Iterable[Point2]:
 
 
 def get_largest_square(points: Iterable[Point2]) -> int:
-    a, b = max(distinct_combinations(points, 2), key=lambda pair: pair[0].area(pair[1]))
+    a, b = max(distinct_combinations(points, 2), key=lambda pair: Rectangle.get_area(pair[0], pair[1]))
     return abs(a.x - b.x + 1) * abs(a.y - b.y + 1)
 
+
+def get_shape_directions(points: list[Point2]) -> int:
+    points = prepend(points[-1], points)
+    dots = (sign((c - p1).cross_sign(p2 - c)) for p1, c, p2 in triplewise(points))
+    return sign(sum(dots))
+
+
+def get_edge_points(points: list[Point2]) -> set[Point2]:
+    return set(flatten(Rectangle.get_points_inside(p1, p2) for p1, p2 in pairwise(prepend(points[-1], points))))
+
+
+def find_inside_seed(points: list[Point2], direction: int) -> Point2:
+    for p1, c, p2 in triplewise(prepend(points[-1], points)):
+        if p1.x == p2.x or p1.y == p2.y: continue  # there is no 'inside' point
+        if sign((c - p1).cross_sign(p2 - c)) != direction: continue  # the corner is facing outward
+        return p1 + (p2 - p1) // 2
+    raise Exception(f"Could not find inside seed: {points}")
+
+
+def flood_fill_insides(edges: set[Point2], inside_seed: Point2) -> set[Point2]:
+    stack = [inside_seed]
+    insides = set()
+
+    while len(stack) > 0:
+        point = stack.pop()
+        insides.add(point)
+        for n in point.get_neighbours():
+            if n in insides or n in edges: continue
+            stack.append(n)
+
+    return insides
+
+
+def get_valid_rectangles(points: list[Point2], edges: set[Point2]) -> Iterable[Rectangle]:
+    rectangles = (Rectangle(p[0], p[1]) for p in distinct_combinations(points, 2))
+    return filter(lambda r: all(p in edges for p in r.points_inside()), rectangles)
+
+
 def get_largest_square_connected(points: Iterable[Point2]) -> int:
-    partitions =
-    a, b = max(distinct_combinations(points, 2), key=lambda pair: pair[0].area(pair[1]))
-    return abs(a.x - b.x + 1) * abs(a.y - b.y + 1)
+    points = list(points)
+    direction = get_shape_directions(points)
+    edges = get_edge_points(points)
+    insides = flood_fill_insides(edges, find_inside_seed(points, direction))
+    valid_rectangles = get_valid_rectangles(points, edges | insides)
+    return max(map(Rectangle.area, valid_rectangles))
--- y2025/d9/test.py
+++ y2025/d9/test.py
@@ -1,18 +1,23 @@
 import os
 import unittest
 
-from y2025.d9.main import get_largest_square, parse_inputs
+from y2025.d9.main import get_largest_square, parse_inputs, get_largest_square_connected
 
 TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'input_test.txt')
 LIVE_FILENAME = os.path.join(os.path.dirname(__file__), 'input.txt')
 
+
 def solve_part_one(file: os.PathLike):
     inputs = parse_inputs(file)
-    total = get_largest_square(inputs)
-    return total
+    largest = get_largest_square(inputs)
+    return largest
+
 
 def solve_part_two(file: os.PathLike):
-    pass
+    inputs = parse_inputs(file)
+    largest = get_largest_square_connected(inputs)
+    return largest
+
 
 class Test(unittest.TestCase):
     def test_part_one(self):
@@ -22,10 +27,10 @@ class Test(unittest.TestCase):
         self.assertEqual(4755429952, solve_part_one(LIVE_FILENAME))
 
     def test_part_two(self):
-        self.assertEqual(25272, solve_part_two(TESTDATA_FILENAME))
+        self.assertEqual(24, solve_part_two(TESTDATA_FILENAME))
 
     def test_part_two_live(self):
-        self.assertEqual(8133642976, solve_part_two(LIVE_FILENAME))
+        self.assertEqual(0, solve_part_two(LIVE_FILENAME))
 
 
 if __name__ == "__main__":

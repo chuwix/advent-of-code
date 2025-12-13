d5fffbe3dfd2bd12cb0e3552fd0a104817bb4633
Martin Milota <martin.milota@chargedmonkey.com>
2025-12-13 19:55:32 +0100

    more math tools

--- tools/math.py
+++ tools/math.py
@@ -1,7 +1,9 @@
 import math
-from itertools import combinations
+from itertools import combinations, combinations_with_replacement
 from typing import Any, SupportsAbs, Generator
 
+def identity(x):
+    return x
 
 def sign(x: SupportsAbs) -> int:
     return int(math.copysign(1, x))
@@ -10,4 +12,11 @@ def sign(x: SupportsAbs) -> int:
 def all_combinations(iterable: list[Any]) -> Generator[tuple[Any, ...], None, None]:
     for i in range(1, len(iterable) + 1):
         for comb in combinations(iterable, i):
+            yield comb
+
+def all_combinations_with_replacement(iterable: list[Any], up_to_size: int, *, include_empty_set: bool = False) -> Generator[tuple[Any, ...], None, None]:
+    start = 1
+    if include_empty_set: start = 0
+    for i in range(start, up_to_size + 1):
+        for comb in combinations_with_replacement(iterable, i):
             yield comb
